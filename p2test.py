b8663bc3d6371a8d19a7f3342aca8ed2a06d47bf
Martin Dousa <martindousa186@gmail.com>
2021-04-22 22:50:16 +0200

    corrected help

--- p2test.py
+++ p2test.py
@@ -343,6 +343,7 @@ def printHelp():
           '\t-t | --time\thow long the test should run\n'
           '\t-s | --strict\thas some extra rules, that should not be necessary\n'
           '\t-w | --wait\twait for the process to finish, even if it is stuck\n'
+          '\t-F | --full\tadds a few test cases with more extreme arguments\n'
           '\t--help\tprint out help\n')
 
 def main():
